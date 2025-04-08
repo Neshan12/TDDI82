68836927a9303a5c48c086d68e22d8b0a0622013
Neshan12 <45181385+Neshan12@users.noreply.github.com>
2025-04-08 13:47:18 +0200

    Update main.cc

--- textredigering/main.cc
+++ textredigering/main.cc
@@ -6,6 +6,8 @@
 #include <sstream>
 #include <iterator>
 #include <map>
+#include <iomanip>
+#include <unordered_map>
 
 std::vector<std::string> read_file(std::string const& filename)
 {
@@ -86,20 +88,77 @@ void replace(std::string const& words, std::vector<std::string> & text)
     // Får in parameter typ: hej+då byt ut hej mot då boom ANVÄND std::replace
 }
 
-// std::map<std::string, int> get_frequency(std::vector<std::string> const& text)
-// {
-//     std::map<std::string, int> tmp{};
-//     std::vector<std::string> unique{};
-//     std::copy(std::unique(text), text.end());
-//     // Gör copy till ny vector med all unika ord
-//     // Räkna antal av varje ord till en int vector
-//     // Sortera unique vectorn med hjälp av int vectorn
-
-// }
-
-void print_frequency(std::map<std::string, int>)
+std::map<std::string, int> get_frequency(std::vector<std::string> const& text)
 {
+    std::map<std::string, int> tmp{};
+    std::vector<std::string> unique{};
 
+    // Gör copy till ny vector med all unika ord
+    std::unique_copy(text.begin(), text.end(), std::back_inserter(unique));
+
+    // Räkna antal av varje ord till en int vector
+    for (auto item : unique)
+    {
+        tmp[item] = std::count(text.begin(), text.end(), item);
+    }
+    return tmp;
+}
+
+void print_frequency(std::map<std::string, int> const& items, 
+    std::vector<std::string> const& text)
+{
+    int max_length
+    {
+        (*std::max_element(text.begin(), text.end(),
+        [](std::string const& lhs, std::string const& rhs)
+        {
+        return lhs.length() < rhs.length();
+        })).length()
+    };
+
+    // Omvandla map till vektor för att kunna sortera
+    /*
+        Vi försökte först använda oss av en unordered_map men fastande
+        AI gav oss tipset att skapa en vektor med pairs och därefter sortera
+        den med hjälp av andra värdet i paret.
+    */
+    std::vector<std::pair<std::string, int>> sorted_items(items.begin(), items.end());
+
+    std::sort(sorted_items.begin(), sorted_items.end(),
+    [](std::pair<std::string, int> const& lhs, std::pair<std::string, int> const& rhs)
+    {
+        return lhs.second < rhs.second;
+    });
+
+    std::reverse(sorted_items.begin(), sorted_items.end());
+
+    std::for_each(sorted_items.begin(), sorted_items.end(),
+    [&max_length](const std::pair<const std::string, int>& element) 
+    {
+        std::cout << std::right << std::setw(max_length) << element.first << " " ;
+        std::cout << element.second << std::endl;
+    });
+}
+
+
+void print_table(std::map<std::string, int> const& items,
+                 std::vector<std::string> const& text)
+{
+    int max_length
+    {
+        (*std::max_element(text.begin(), text.end(),
+        [](std::string const& lhs, std::string const& rhs)
+        {
+            return lhs.length() < rhs.length();
+        })).length()
+    };
+
+    std::for_each(items.begin(), items.end(),
+    [&max_length](const std::pair<const std::string, int>& element) 
+    {
+        std::cout << std::left << std::setw(max_length) << element.first << " " ;
+        std::cout << element.second << std::endl;
+    });
 }
 
 using namespace std;
@@ -127,11 +186,11 @@ int main(int argc, char* argv[])
         }
         else if (flags.at(i) == "--frequency")
         {
-
+            print_frequency(get_frequency(text), text);
         }
         else if (flags.at(i) == "--table")
         {
-
+            print_table(get_frequency(text), text);
         }
     }
 
